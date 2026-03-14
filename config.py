9e9c1ce013800a56116f4caf6c3820acacc5dcb1
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-14 22:58:25 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# config.py
+API_ID = 24752047          # استبدله بـ api_id الخاص بك
+API_HASH = "5b8a468627791bdd36a8c361913b0b72"    # استبدله بـ api_hash الخاص بك
+BOT_TOKEN = "8721155986:AAHdipR_Xg6YUebhq_FWU3_oeHjyNdePT_c"  # استبدله بـ توكن البوت
